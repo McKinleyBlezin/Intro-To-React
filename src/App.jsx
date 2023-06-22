01fc148fd5ec08c0f5034efacce9d6963716d3be
McKinley Blezin <mblezin.usca@gmail.com>
2023-06-21 22:09:15 -0400

    Tired to add a select feature for puppies

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,24 @@ import { puppyList } from './puppy-data/data'
 
 function App() {
 
-const [puppies, setPuppies] = useState(puppyList)
+const [puppies, setPuppies] = useState(puppyList);
+const [featPupId, setFeatPupId] = useState(null);
 { 
-  puppies.map((puppy) => {
-    
-    console.log("puppyList: ", puppyList);
-    return <p>{puppy.name}</p>
+  puppies.map((puppyList) => { 
+    console.log("puppyList:", puppyList);
+
+    function handleClick() {
+   
+    }
+    // const featurePup = puppies.find(puppiesList) => puppyList.id === featPupId
+    console.log (featurePup)
+
+    return 
+    <div>
+    <p onClick={handleClick} key={puppyList.id}>
+            {puppyList.name}</p>
+    <p>{puppyList.name}</p>
+    </div>
   })
 }
 
