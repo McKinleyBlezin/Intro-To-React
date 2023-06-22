394b5045028bde1e76e319bcbe0e681ac77c6f07
McKinley Blezin <mblezin.usca@gmail.com>
2023-06-21 22:12:23 -0400

    Import and added CSS

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Counter from './components/Counter'
 import { puppyList } from './puppy-data/data'
-
+import './path-to-css.css' //Is this not the same as line 2 trying to import/link the app.css together?
 
 function App() {
 
