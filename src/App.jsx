c85c57e17bb2501e448868e7b06f2acf9479aa31
McKinley Blezin <mblezin.usca@gmail.com>
2023-06-22 20:01:02 -0400

    fixed mapping through puppyList

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,37 @@ import { useState } from 'react'
 import './App.css'
 import Counter from './components/Counter'
 import { puppyList } from './puppy-data/data'
-import './path-to-css.css' //Is this not the same as line 2 trying to import/link the app.css together?
 
 function App() {
 
 const [puppies, setPuppies] = useState(puppyList);
 const [featPupId, setFeatPupId] = useState(null);
-{ 
-  puppies.map((puppyList) => { 
-    console.log("puppyList:", puppyList);
 
-    function handleClick() {
+//     function handleClick() {
    
-    }
-    // const featurePup = puppies.find(puppiesList) => puppyList.id === featPupId
-    console.log (featurePup)
+//     }
+//     // const featurePup = puppies.find(puppiesList) => puppyList.id === featPupId
+//     console.log (featurePup)
 
-    return 
-    <div>
-    <p onClick={handleClick} key={puppyList.id}>
-            {puppyList.name}</p>
-    <p>{puppyList.name}</p>
-    </div>
-  })
-}
+//     return 
+//     <div>
+//     <p onClick={handleClick} key={puppyList.id}>
+//             {puppyList.name}</p>
+//     <p>{puppyList.name}</p>
+//     </div>
+//   })
+// }
 
   return (
     <>
-   
+   {puppies.map((puppy) => {
+
+    return 
+      <p>   
+        {puppy.name} {puppy.email}
+      </p> 
+
+   })} 
     </>
   )
 }
