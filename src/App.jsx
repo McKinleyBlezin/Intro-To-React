c97b88a970c54484bb43385aa5dbb69f2234749b
McKinley Blezin <mblezin.usca@gmail.com>
2023-06-23 18:32:46 -0400

    clean up the good and add onClick

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,39 @@
-import { useState } from 'react'
-import './App.css'
-import Counter from './components/Counter'
-import { puppyList } from './puppy-data/data'
+import { useState } from "react";
+import "./App.css";
+import Counter from "./components/Counter";
+import { puppyList } from "./puppy-data/data";
 
 function App() {
-
-const [puppies, setPuppies] = useState(puppyList);
-const [featPupId, setFeatPupId] = useState(null);
-// { 
-//   puppies.map((puppyList) => { 
-//     console.log("puppyList:", puppyList);
-
-//     function handleClick() {
-   
-//     }
-//     // const featurePup = puppies.find(puppiesList) => puppyList.id === featPupId
-//     console.log (featurePup)
-
-//     return 
-//     <div>
-//     <p onClick={handleClick} key={puppyList.id}>
-//             {puppyList.name}</p>
-//     <p>{puppyList.name}</p>
-//     </div>
-//   })
-// }
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
 
   return (
     <>
-   {puppies.map((puppy) => {
+      {puppies.map((puppy) => {
+        return (
+          <p
+            onClick={() => {
+              setFeatPupId(puppy.id);
+            }}
+          >
+            {" "}
+            {puppy.name} {puppy.email}
+          </p>
+        );
+      })}
 
-return <p> {puppy.name} {puppy.email}
-</p>
-
-   })} 
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ol>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ol>
+        </div>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
